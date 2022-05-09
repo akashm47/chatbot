b97041fb9a316e2dea7172d2c5d54bbc9f7b9412
Akash Mahapatra <90324160+akashm47@users.noreply.github.com>
2022-05-09 19:53:40 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#send-btn").on("click", function(){
+        $value = $("#data").val();
+        $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+        $(".form").append($msg);
+        $("#data").val('');
+        
+        // start ajax code
+        $.ajax({
+            // url: URL("message.php"),
+            type: 'POST',
+            data: 'text='+$value,
+            success: function(result){
+                $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                $(".form").append($replay);
+                // when chat goes down the scroll bar automatically comes to the bottom
+                $(".form").scrollTop($(".form")[0].scrollHeight);
+            }
+        });
+    });
+});
